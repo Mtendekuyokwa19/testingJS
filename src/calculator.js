72144a238d2eb1737231fe4c76b1e0f188e03bf8
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2024-04-02 22:14:43 +0200

    feature: writing functions for calculator

---
+++ src/calculator.js
@@ -0,0 +1,21 @@
+function multiply (firstNumber,secondNumber){
+
+    return firstNumber*secondNumber
+}
+
+function add(firstNumber,secondNumber){
+
+    return firstNumber+secondNumber;
+}
+
+function subtract(firstNumber,secondNumber){
+
+    return firstNumber-secondNumber;
+}
+
+function divide(numerator,denominator) {
+
+    return numerator/denominator
+}
+
+module.exports={multiply,divide,subtract,add}
--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,8 +1,8 @@
-const { test } = require('picomatch');
 
-const { default: expect } = require('expect');
 
-var calculations=require('../calculator');
+let calculations=require('../calculator.js');
+
+
 test("addition of 1 and 2",()=>{
 
     expect(calculations.add(1,2)).toBe(3)
@@ -27,6 +27,7 @@ test("multiply 3 and 9",()=>{
 
 test("multiply 2 and 3 ",()=>{
     expect(calculations.multiply(2,3)).toBe(6)
+    
 })
 
 test ("division of 9 and 3",()=>{
