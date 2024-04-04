5aafb05c1c413f0ae3b441db5360b8e3ffb6001f
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2024-04-04 03:54:53 +0200

    feature: creating of array summerize obj

---
+++ src/analyzeArray.js
@@ -0,0 +1,27 @@
+function analyzeArray(array) {
+ let summaryObj={
+    average:null,
+    min:array[0],
+    max:array[0],
+    length:null
+ }
+
+ summaryObj.length=array.length;
+ let sum=0;
+ for (let i = 0; i < array.length; i++) {
+sum+=array[i];
+if(summaryObj.min>array[i]){
+    summaryObj.min=array[i]
+}
+
+if(summaryObj.max<array[i]){
+    summaryObj.max=array[i]
+}
+ }
+
+ summaryObj.average=sum/summaryObj.length
+
+ return summaryObj
+}
+
+module.exports=analyzeArray
---
+++ src/tests/analyzeArray.test.js
@@ -0,0 +1,21 @@
+
+let analyzeArray=require('../analyzeArray')
+
+
+test("array [2,4,1,5]",()=>{
+
+    expect(analyzeArray([2,4,1,5])).toStrictEqual({  average: 3,
+        min: 1,
+        max: 5,
+        length: 4})
+})
+
+test("array [1,8,3,4,2,6]",()=>{
+expect(analyzeArray([1,8,3,4,2,6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  })
+}
+)
