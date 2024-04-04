4c54597ab109c8ce419b66deffecf6b6fca32c85
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2024-04-04 03:09:54 +0200

    feature: implementing reverse string

---
+++ src/reverse.js
@@ -0,0 +1,12 @@
+function reverse(word) {
+    word=word.split("");
+let reversedWord="";
+    for (let i = word.length-1; i >-1 ; i--) {
+      reversedWord+=word[i]
+        
+    }
+  
+return reversedWord;
+}
+
+module.exports=reverse;
---
+++ src/tests/reverse.test.js
@@ -0,0 +1,13 @@
+
+var reverse=require('../reverse');
+const { default: expect } = require('expect');
+
+test("Otis to sitO",()=>{
+
+    expect(reverse("Otis")).toMatch("sitO")
+})
+
+test("jack to kcaj",()=>{
+
+    expect(reverse("jack")).toMatch("kcaj")
+})
