1f8482d386b33f0b4d7d1d0f0e865e767fc4c199
mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2024-04-09 19:17:48 +0200

    Revert "fix : removing of the equivalent letter tests"
    This reverts commit 2a09ce9f94257dadba056b75e72385b750605842.

--- src/tests/caeser.test.js
+++ src/tests/caeser.test.js
@@ -1,5 +1,7 @@
 
-const caeser=require('../caeser.js');
+
+const { test } = require('picomatch');
+const caeser=require('../caeser');
 
 test("changing of word jack to MDFN",()=>{
 
@@ -12,7 +14,14 @@ test ("changing the word speed to YVKKJ",()=>{
     expect(caeser.cipher("speed",6)).toBe("YVKKJ")
 })
 
+test("return equivalent letter. A => C",()=>{
 
+    expect(caeser.equivalent("A",3)).toBe("c")
+})
+
+test("return equivalent letter B=>F",()=>{
+    expect(caeser.equivalent("B",4)).toBe("F")
+})
 
 test("check if its array",()=>{
 
@@ -26,9 +35,9 @@ test ("check if its a string array [2,4,5,2]",()=>{
 
 test("bring out the alphabet for the letters",()=>{
 
-    expect(caeser.moveByShiftKey(4)).toMatch("E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,A,B,C,D")
+    expect(caeser.moveByShiftKey(3)).toBe("EFGHIJKLMNOPQRSTUVWXYZABCD")
 })
 
 test("bring out the alphabet for letters 2",()=>{
-    expect(caeser.moveByShiftKey(6)).toMatch("G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,A,B,C,D,E,F")
+    expect(caeser.moveByShiftKey(8)).toBe("GHIJKLMNOPQRSTUVWXYZABCDEF")
 })
